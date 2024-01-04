2d1069ec7bb2b89b725791ba95f5226d188172c4
batuhan içöz <batuhan@icoz.co>
2024-01-04 16:38:54 +0100

    add facebook check for mqtt, add filter for ws msgs

--- web/src/js/components/FlowView/Messages.tsx
+++ web/src/js/components/FlowView/Messages.tsx
@@ -16,9 +16,12 @@ type MessagesPropTypes = {
 
 export default function Messages({ flow, messages_meta }: MessagesPropTypes) {
     const dispatch = useAppDispatch();
+    const [filter, setFilter] = useState('all')
+    const isFacebookChat = (flow.type === 'http' && flow.request.host.includes("edge-chat"))
+    const defaultView = isFacebookChat ? 'MQTT' : 'Auto'
 
     const contentView = useAppSelector(
-        (state) => state.ui.flow.contentViewFor[flow.id + "messages"] || "Auto"
+        (state) => state.ui.flow.contentViewFor[flow.id + "messages"] || defaultView
     );
     let [maxLines, setMaxLines] = useState<number>(
         useAppSelector((state) => state.options.content_view_lines_cutoff)
@@ -52,6 +55,34 @@ export default function Messages({ flow, messages_meta }: MessagesPropTypes) {
         <div className="contentview">
             <div className="controls">
                 <h5>{messages_meta.count} Messages</h5>
+                <div>
+                    <input
+                        type="radio"
+                        id="all"
+                        name="filter"
+                        checked={filter === 'all'}
+                        onChange={() => setFilter('all')}
+                    />
+                    <label htmlFor="all">All</label>
+
+                    <input
+                        type="radio"
+                        id="onlySent"
+                        name="filter"
+                        checked={filter === 'onlySent'}
+                        onChange={() => setFilter('onlySent')}
+                    />
+                    <label htmlFor="onlySent">Only Sent</label>
+
+                    <input
+                        type="radio"
+                        id="onlyReceived"
+                        name="filter"
+                        checked={filter === 'onlyReceived'}
+                        onChange={() => setFilter('onlyReceived')}
+                    />
+                    <label htmlFor="onlyReceived">Only Received</label>
+                </div>
                 <ViewSelector
                     value={contentView}
                     onChange={(cv) =>
@@ -60,9 +91,10 @@ export default function Messages({ flow, messages_meta }: MessagesPropTypes) {
                 />
             </div>
             {messages.map((d: ContentViewData, i) => {
-                const className = `fa fa-fw fa-arrow-${
-                    d.from_client ? "right text-primary" : "left text-danger"
-                }`;
+                if (filter === 'onlySent' && !d.from_client) return null
+                if (filter === 'onlyReceived' && d.from_client) return null
+                const className = `fa fa-fw fa-arrow-${d.from_client ? "right text-primary" : "left text-danger"
+                    }`;
                 const renderer = (
                     <div key={i}>
                         <small>
@@ -80,7 +112,7 @@ export default function Messages({ flow, messages_meta }: MessagesPropTypes) {
                 );
                 maxLines -= d.lines.length;
                 return renderer;
-            })}
+            }).filter(Boolean)}
         </div>
     );
 }
